aab911d80a87ff005a12f656e56951ce7d4fa929
Brittany Russell <sincerelybr@gmail.com>
2020-03-03 15:28:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,11 @@ def introduction(name)
   puts "Hi, my name is #{name}.\n"
 end
 
+<<<<<<< HEAD
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+=======
+def introduction(name)
+  puts "Hi, my name is #{name}.\n"
+>>>>>>> 0c927b4527eec666975e00d02ad4d3e6ab9d2bbf
 end
